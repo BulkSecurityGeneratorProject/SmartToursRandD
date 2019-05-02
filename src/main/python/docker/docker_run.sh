c218a671cbc284242e73f2f7cbaeb932bb8c3d53
Thomas Bigger <thomas@performanceactive.com>
2019-05-02 15:46:13 +0100

    update of run script

--- src/main/python/docker/docker_run.sh
+++ src/main/python/docker/docker_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker run -p 8000:8000 \
-    -v ~/volumes/smart-tours-ml:latest/trained_models:/usr/src/app/app/trained_models \
-    -v ~/volumes/smart-tours-ml:latest/data:/usr/src/app/app/data \
+    -v ~/volumes/smart-tours-ml/trained_models:/usr/src/app/app/trained_models \
+    -v ~/volumes/smart-tours-ml/data:/usr/src/app/app/data \
     smart-tours-ml:latest
