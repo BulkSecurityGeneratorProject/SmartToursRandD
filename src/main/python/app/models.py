4692cccecea1a070f2e671922562094b7ff6acf7
Thomas Bigger <thomas@performanceactive.com>
2019-05-03 11:15:28 +0100

    accepting a body with id and distance and returning back probability of purchase

--- src/main/python/app/models.py
+++ src/main/python/app/models.py
@@ -1,39 +1,33 @@
 import turicreate as tc
 
-# constants
 data_folder = 'app/data'
 data_file = 'trx_data.csv'
 
 model_folder = 'app/trained_models'
 model_name = 'smart-tours-model'
 
-
 class LogisticClassifier:
     def train(self):
-        # Load the data (From an S3 bucket)
         data = tc.SFrame(data_folder + '/' + data_file)
-
         print('Creating model')
         model = tc.logistic_classifier.create(
             data, target='actionTaken', features=['userDistance'])
-
         print("Coefficients...")
         print(model.coefficients)
-
         print("Evaluations...")
         print(model.evaluate(data))
-
         model.save(model_folder + '/' + model_name)
 
-    def query(self, distances):
-        distances = distances.split('|')
-        lst = list(map(float, distances))
-        test = tc.SFrame({'userDistance': lst, })
+    def query(self, inputData):
+        ids = list()
+        distances = list()
+        for index, thisItem in enumerate(inputData):
+            ids.append(thisItem['itemId'])
+            distances.append(thisItem['distance'])
+
+        test = tc.SFrame({'userDistance': distances, })
 
         model = tc.load_model(model_folder + '/' + model_name)
-
-        print("Predictions...")
-        # get out true or false for each
 
         pActionTaken = model.predict(test)
         pProbabilities = model.predict(test, output_type='probability')
@@ -43,8 +37,9 @@ class LogisticClassifier:
         for index, actionTaken in enumerate(pActionTaken):
             obj = {}
             obj['index'] = index
-            obj['distance'] = test['userDistance'][index]
-            obj['actionTaken'] = actionTaken
+            obj['itemId'] = ids[index]
+            obj['distance'] = distances[index]
+            obj['actionTaken'] = actionTaken.lower() == 'true'
             obj['probability'] = pProbabilities[index]
             obj['margin'] = pMargins[index]
             data.append(obj)
--- src/main/python/app/views.py
+++ src/main/python/app/views.py
@@ -3,16 +3,18 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from rest_framework.parsers import JSONParser
+
 
 class LogisticRegressionView(APIView):
-    def post(self, request, format=None):
+    def put(self, request):
         LogisticClassifier().train()
         return Response(status=status.HTTP_200_OK)
 
-    def get(self, request):
+    def post(self, request):
+        inputData = JSONParser().parse(request)
         try:
-            distances = request.query_params['distances']
+            response = LogisticClassifier().query(inputData)
+            return Response(response, status=status.HTTP_200_OK)
         except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        response = LogisticClassifier().query(distances)
-        return Response(response, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
