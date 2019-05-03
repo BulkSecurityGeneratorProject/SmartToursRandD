a5eb5f9ba708a7c15812a6f3e21d502bfcb3c8a8
Thomas Bigger <thomas@performanceactive.com>
2019-05-03 12:02:59 +0100

    put an upper and lower bound in so as to include trues and flases from the get go, otherwise an error from the logistic regressor

--- src/main/java/com/pa/twb/service/ext/processing/MachineLearningTrainerService.java
+++ src/main/java/com/pa/twb/service/ext/processing/MachineLearningTrainerService.java
@@ -7,8 +7,12 @@ import com.pa.twb.service.ext.processing.dto.csv.CsvDataDTO;
 import com.pa.twb.service.mapper.ext.ExtAttractionPurchaseMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
@@ -36,10 +40,27 @@ public class MachineLearningTrainerService {
     public void train() {
         List<AttractionPurchase> result = extAttractionPurchaseRepository.findAll();
         List<CsvDataDTO> csvList = extAttractionPurchaseMapper.entityListToCsv(result);
+
+        CsvDataDTO initialTrueCsv = new CsvDataDTO();
+        initialTrueCsv.setUserDistance(0d);
+        initialTrueCsv.setActionTaken(true);
+
+        CsvDataDTO initialFalseCsv = new CsvDataDTO();
+        initialFalseCsv.setUserDistance(10000d);
+        initialFalseCsv.setActionTaken(false);
+
+        csvList.add(initialTrueCsv);
+        csvList.add(initialFalseCsv);
+
         try {
             csvService.write(csvList);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        RestTemplate restTemplate = new RestTemplate();
+        URI uri = URI.create("http://127.0.0.1:8000/logistic-regression/");
+        ResponseEntity<Void> responseEntity =
+            restTemplate.exchange(uri, HttpMethod.PUT, null, Void.class);
+
     }
 }
